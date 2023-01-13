1deb9635005bba2e4f508532bb90b08a4a54fa1b
Brayan Sierra <bd68@misena.edu.co>
2023-01-12 20:47:46 -0500

    Se crea proyecto e-comerce javascript

---
+++ stock.js
@@ -0,0 +1,42 @@
+const prendas =
+[
+{
+    id:1,
+    nombre: "saco",
+    tipo: "saco",
+    descripcion: "saco estilo deportivo y aesthetic, te gustara",
+    costo: 65000,
+    talla: "M",
+    cantidad:0
+},
+
+{
+    id: 2,
+    nombre: "t-shirt",
+    tipo: "camisa",
+    descripcion: "camisa aesthetic",
+    costo: 50000,
+    talla: "M",
+    cantidad: 0
+},
+
+{
+    id:3,
+    nombre:"pantalon",
+    tipo:"pantalon",
+    descripcion: "pantalon large",
+    costo: 70000,
+    talla: "28",
+    cantidad: 0
+},
+
+{
+    id:4,
+    nombre:"leñadora estilo oversize",
+    tipo:"camiseta leñadora grande, oversize",
+    descripcion:"camiseta oversize aesthetic",
+    costo: 120000,
+    talla: "L",
+    cantidad: 0
+},
+]
---
+++ tienda.js
@@ -0,0 +1,107 @@
+// array carro de compra
+const carro = []
+
+const ordenarMenorMayor = () => {
+    prendas.sort((a, b) => a.costo - b.costo)
+    mostrarListaOrdenada()
+};
+
+const ordenarMayorMenor = () => {
+    prendas.sort((b, a) => a.costo - b.costo)
+    mostrarListaOrdenada()
+};
+
+const mostrarListaOrdenada = () => {
+    const listadePrendas = prendas.map(prenda => {
+        return '- ' + prenda.nombre + ' $' + prenda.costo
+    })
+    alert('lista de costos:' + '\n\n' + listadePrendas.join('\n'))
+    compradePrendas(listadePrendas)
+};
+
+const compradePrendas = (listadePrendas) => {
+    let prendasNombre = ''
+    let prendasCantidad = 0
+    let otraPrenda = false
+
+    do {
+        prendasNombre = prompt('¿que prendas deseas comprar🤗?' + '\n\n' + listadePrendas.join('\n'))
+        prendasCantidad = parseInt(prompt('¿Cuantas prendas quieres comprar🤨?'))
+
+        const prenda = prendas.find(prenda => prenda.nombre.toLowerCase() === prendasNombre.toLowerCase())
+
+        if (prenda) {
+            addOfCar(prenda, prenda.id, prendasCantidad)
+        } else {
+            alert('la prenda no esta en inventario por ahora😣')
+        }
+
+        otraPrenda = confirm('¿agregaras algo más?')
+    } while (otraPrenda);
+
+    confirmBuy()
+}
+
+const addOfCar = (prenda, prendaId, prendaCantidad) => {
+    const prendaIgual = carro.find(prenda => prenda.id === prendaId)
+    if (!prendaIgual) {
+        prenda.cantidad += prendaCantidad
+        carro.push(prenda)
+    } else {
+        prendaIgual.cantidad += prendaCantidad
+    }
+};
+
+const sacarPrendaCarro = (nombrePrendaAEliminar) => {
+    carro.forEach((prenda, index) => {
+        if (prenda.nombre.toLowerCase() === nombrePrendaAEliminar.toLowerCase()) {
+            if (prenda.cantidad > 1) {
+                prenda.cantidad--
+            } else {
+                carro.splice(index, 1)
+            }
+        }
+    })
+    confirmBuy()
+};
+
+const confirmBuy = () => {
+    const listadePrendas = carro.map(prenda => {
+        return '- ' + prenda.nombre + ' | Cantidad: ' + prenda.cantidad
+    })
+
+    const isCheckout = confirm('checkout:'
+        +'\n\n' + listadePrendas.join('\n')
+        +'\n\n si quieres continuar dale "aceptar"🙂 sino "cancelar para eliminar una prenda del carro"🙄'
+    )
+
+    if (isCheckout) {
+        finalizarBuy(listadePrendas)
+    } else {
+        const nombrePrendaAEliminar = prompt('ingrese el nombre de la prenda a eliminar 😕')
+        sacarPrendaCarro(nombrePrendaAEliminar)
+    }
+};
+
+const finalizarBuy = (listadePrendas) =>{
+    const cantidadTotal = carro.reduce((acc, item) => acc + item.cantidad, 0)
+    const costoTotal = carro.reduce((acc, item) => acc + (item.cantidad * item.costo), 0)
+    alert(
+        +'\n\n' + listadePrendas.join('\n')
+        +'\n\nTotal de prendas: '+cantidadTotal
+        +'\n\nEl Total de tus prendas compradas es: '+costoTotal.toLocaleString()
+        +'\n\nGracias por tu compra!'
+    )
+};
+
+const comprar = () =>{
+    const prendasBaratas = confirm('¿quieres que la lista de compra este ordenada del mas barato al mas caro?')
+
+    if(prendasBaratas){
+        ordenarMenorMayor()
+    }else{
+        ordenarMayorMenor
+    }
+};
+
+comprar()
